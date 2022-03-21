2e47ad1a50971f23e2758d0d0adacfea1e6f5485
Vyollet <te>
2022-03-21 21:29:23 +0300

    Add resources

--- lib/data/dataFiles.dart
+++ lib/data/dataFiles.dart
@@ -20,4 +20,16 @@ List<Store> storeItems =[
     itemName: "OpenVPN",
     itemImage: "https://raw.githubusercontent.com/dperson/openvpn-client/master/logo.png",
   ),
+  Store.items(
+    itemName: "BitBucket",
+    itemImage: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Bitbucket-blue-logomark-only.svg/1200px-Bitbucket-blue-logomark-only.svg.png",
+  ),
+  Store.items(
+    itemName: "Gitlab",
+    itemImage: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/GitLab_logo.svg/1200px-GitLab_logo.svg.png",
+  ),
+  Store.items(
+    itemName: "GitHub",
+    itemImage: "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+  ),
 ];
